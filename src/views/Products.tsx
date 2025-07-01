2e97020590de4a11fa864a56c48db2672da15048
Luis Isaac Cuautle Delgadillo <cuautle.tron@gmail.com>
2025-07-01 11:39:11 -0600

    Trabajando en el layout

---
+++ src/views/Products.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+
+export default function Products() {
+  return (
+    <>
+        <div className="flex justify-between">
+
+        </div>
+    </>
+  )
+}
