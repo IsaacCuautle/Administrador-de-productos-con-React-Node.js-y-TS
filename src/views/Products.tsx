fc284548aa44aec599058540b7cd7b3e8a932de7
Luis Isaac Cuautle Delgadillo <cuautle.tron@gmail.com>
2025-07-24 10:12:25 -0600

    Fin del proyecto

--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { Link, useLoaderData, type ActionFunctionArgs } from "react-router-dom";
 
 import { getProducts, updateAvailability } from "../services/ProductService";
